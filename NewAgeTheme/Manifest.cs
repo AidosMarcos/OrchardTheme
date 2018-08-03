e3b709b13431e185005052ccf4eea189f5d89f34
Marcos Aidos <marcos.aidos@protonmail.com>
2018-08-03 18:22:31 +0100

    NewThemeCreated

---
+++ NewAgeTheme/Manifest.cs
@@ -0,0 +1,10 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "NewAge Theme",
+    Author = "Marcos Aidos",
+    Website = "http://orchardproject.net",
+    Version = "2.0.0",
+    Description = "A theme adapted for agency websites.",
+    Tags = [ "LandingPage" ]
+)]
